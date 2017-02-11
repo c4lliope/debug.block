24a705735f28ac6bac431880d4e79f1b45d0fc23
Grayson Wright <wright.grayson@gmail.com>
2017-02-10 22:51:21 -0800

    Refactor assemble.rb

--- assemble.rb
+++ assemble.rb
@@ -2,17 +2,19 @@ require "json"
 
 class Assemble
   def self.current_run
-    Struct.
-      new(:block_name, :input).
-      new(
-        ENV.fetch("ASSEMBLE_BLOCK_NAME"),
-        JSON.parse(File.read(File.join(assemble_dir, "input.json"))),
-      )
+    Run.new(
+      ENV.fetch("ASSEMBLE_BLOCK_NAME"),
+      ENV.fetch("ASSEMBLE_DIR"),
+    )
   end
 
-  private
+  class Run < Struct.new(:block_name, :assemble_dir)
+    def assemble_dir
+      ENV.fetch("ASSEMBLE_DIR", "/.assemble")
+    end
 
-  def assemble_dir
-    ENV.fetch("ASSEMBLE_DIR", "/.assemble")
+    def input
+      JSON.parse(File.read(File.join(assemble_dir, "input.json")))
+    end
   end
 end
